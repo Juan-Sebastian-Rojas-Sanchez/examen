c383675c42adabdd6b61565da28c0c7f59759973
Juan-Sebastian-Rojas-Sanchez <186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com>
2024-11-05 12:53:12 -0500

    ejercicio 3

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import math
+# Ask the user to enter the grades
+v1 = float(input("Ingrese el primer voltaje: "))
+v2 = float(input("Ingese el segundo voltaje: "))
+v3 = float(input("Ingese el tercer voltaje:"))
 
-def area_triangulo_equilatero(lado):
-    return (math.sqrt(3) / 4) * lado ** 2
+# Calculate the average
+average = (v1 + v2 + v3) / 3
 
-# Ejemplo de uso
-lado = float(input("Ingrese el lado triangulo: "))
-area = area_triangulo_equilatero(lado)
-if area < 1000:
-  print(f"El area del triangulo equilatero es: {area}")
-else:
-    print(f"datos no validos: {area}")
-
+if average < 115:
+  print(f"El voltaje es correcto ya que su promedio es: {average}")
+elif average >115 and average<220:
+    print(f"El voltaje es alto ya que su promedio es: {average}")
+elif average > 220:
+    print(f"Peligro ya que su promedio es: {average}")
